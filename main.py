8b6d192bc64928cf1ba95f7ee316571015af414a
jj-devel <jj.dev3l@gmail.com>
2025-07-15 14:07:42 -0400

    Visual Update: Added comments and cleaned up code to be more presentable and added spacing where needed.

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
 import sys
-from stats import get_book_text, word_count, char_count, sort_char_count
+from stats import get_book_text, word_count, sort_char_count
 
+# Set a startup condition where the program needs two arguments to continue. 
 if len(sys.argv) < 2:
     print("Usage: python3 main.py <path_to_book>")
     sys.exit(1)
 
+# Set the txt used for bookbot to the path given by the user in the second system argument. 
 path_to_book = sys.argv[1]
 
+# Main function that holds the actions for bookbot. 
 def main():
+    # Necessary variables for the functions in stats to work. 
     words = get_book_text(path_to_book).split()
     chars_list = list(get_book_text(path_to_book))
     chars = [char.lower() for char in chars_list]
 
+    # Variables holding the stats functions
     num_words = word_count(words)
     sorted_char_list = sort_char_count(chars)
+
+    # Print the intro to bookbot, along with the {num_words} variable. 
     print(f"""
 ============ BOOKBOT ============
 Analyzing book found at books/frankenstein.txt...\n
@@ -21,8 +28,11 @@ Analyzing book found at books/frankenstein.txt...\n
 Found {num_words} total words\n
 --------- Character Count -------
           """)
+    
+    # Print each character found in the txt, with the corisponding amount of the character found in the txt. Ordered from most to least. 
     for i in range(0, len(sorted_char_list)):
         print(f"{sorted_char_list[i]["char"]}: {sorted_char_list[i]["count"]}")
     print("============= END ===============")
 
+# Run the main function. 
 main()
--- stats.py
+++ stats.py
@@ -1,11 +1,14 @@
+# This takes the user path found in [main.py] and converts it into a string. 
 def get_book_text(filepath):
     with open(filepath) as f:
         book_text = f.read()
     return book_text
 
+# Returns the length of the txt for the total work count. 
 def word_count(words):
     return len(words)
 
+# Adds each individual char to a dictionary and adds the count of it. Ex. {char, count}, etc
 def char_count(chars):
     char_dict = {}
     for char in chars:
@@ -15,9 +18,11 @@ def char_count(chars):
             char_dict[char] += 1
     return char_dict
 
+# Key for the sort function necessary to make the chars in order of largest to smallest amount. 
 def sort_key(list):
     return list["count"]
 
+# Converts the chars in {chars_dict} to individual dictionaries in a list, then ignores the non-alphabetic characters and sorts them from largest to smallest count. 
 def sort_char_count(chars):
     char_dict = char_count(chars)
     char_dict_list = []
